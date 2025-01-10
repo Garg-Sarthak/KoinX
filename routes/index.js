8bc45a36d2410cf4b9aee7d25a9985b122fff5cc
Sarthak Garg <sarthak@Sarthaks-MacBook-Air-2.local>
2025-01-10 21:52:45 +0530

    feature implement - deviation route added

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,33 @@ topRouter.get("/stats", async (req, res) => {
     }
 })
 
+topRouter.get("/deviation", async (req, res) => {
+    const coin_id = req.query.coin;
+    if (!coin_id){
+        res.status(400).json({error : "No coin provided"});
+        return;
+    }
 
+    try{
+        const coinData = await Coin.find({coin_id}).select('current_price last_updated').sort({last_updated : -1}).limit(100);
+
+        if (!coinData){
+            res.status(404).json({error : "Coin not found"});
+            return;
+        }
+
+        let mean = coinData.reduce((a,b) => a + b.current_price, 0) / coinData.length;
+        let deviation = coinData.map(coin => Math.abs(coin.current_price - mean));
+        let stdDev = Math.sqrt(deviation.reduce((a,b) => a + (b**2),0) / deviation.length) ;
+
+
+        res.status(200).json({
+            deviation : stdDev
+        });
+
+    }catch(e){
+        res.status(500).json({error : e.message});
+    }
+})
 
 module.exports = topRouter;
